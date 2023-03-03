5e554e5a85f73b2fa1da7ad346e2c962cedd13da
Mohammed Murshid <murshid434@gmail.com>
2023-03-03 10:21:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      Welcome... Let's build social media application using MERN Stack
+    </div>
+  );
+}
+
+export default App;
