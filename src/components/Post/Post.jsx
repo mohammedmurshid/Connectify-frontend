90eed5643cf67e2217e004cb40b27241f68cebaf
Mohammed Murshid <murshid434@gmail.com>
2023-03-04 22:07:55 +0530

    posts rendering section done

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Post.css";
+import Like from "../../img/like.png";
+import notLike from "../../img/notlike.png";
+import Comment from "../../img/comment.png";
+import Share from "../../img/share.png";
+
+const Post = ({ data }) => {
+  return (
+    <div className="Post">
+      <img src={data.img} alt="" />
+
+      <div className="postReactions">
+        <img src={data.liked ? Like : notLike} alt="" />
+        <img src={Comment} alt="" />
+        <img src={Share} alt="" />
+      </div>
+
+      <span>{data.likes} Likes</span>
+
+      <div className="details">
+        <span>
+          <b>{data.name}</b>
+        </span>
+        <span> {data.desc}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
