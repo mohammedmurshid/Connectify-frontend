d4db3f3932ce14432955d04ebab6c9cec79046c8
Mohammed Murshid <murshid434@gmail.com>
2023-03-24 15:01:27 +0530

    post like functionality

--- src/api/PostRequest.js
+++ src/api/PostRequest.js
@@ -3,3 +3,6 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:4000" });
 
 export const getTimelinePosts = (id) => API.get(`/post/${id}/timeline`);
+
+export const reactPost = (id, userId) =>
+  API.put(`post/${id}/react`, { userId: userId });
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Post.css";
 import Like from "../../img/like.png";
 import notLike from "../../img/notlike.png";
 import Comment from "../../img/comment.png";
 import Share from "../../img/share.png";
 import { useSelector } from "react-redux";
+import { reactPost } from "../../api/PostRequest";
 
 const Post = ({ data }) => {
   const { user } = useSelector((state) => state.authReducer.authData);
+
+  const [liked, setLiked] = useState(data.likes.includes(user._id));
+  const [likes, setLikes] = useState(data.likes.length);
+
+  const handleLike = () => {
+    setLiked((prev) => !prev);
+    reactPost(data._id, user._id); // sending the info to the backend
+    liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1); // unlikes the post if the post is liked and vice versa
+  };
+
   return (
     <div className="Post">
       <img
@@ -16,13 +27,18 @@ const Post = ({ data }) => {
       />
 
       <div className="postReactions">
-        <img src={data.liked ? Like : notLike} alt="" />
+        <img
+          src={liked ? Like : notLike}
+          alt=""
+          style={{ cursor: "pointer" }}
+          onClick={handleLike}
+        />
         <img src={Comment} alt="" />
         <img src={Share} alt="" />
       </div>
 
       <span style={{ color: "var(--gray)", fontSize: "12px" }}>
-        {data.likes} Likes
+        {likes} Likes
       </span>
 
       <div className="details">
