f877732d0f4caed20e8f7c517caafecfacb7628f
Mohammed Murshid <murshid434@gmail.com>
2023-03-27 23:57:28 +0530

    follow and unfollow

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -3,16 +3,22 @@ import "./Posts.css";
 import { useDispatch, useSelector } from "react-redux";
 import Post from "../Post/Post";
 import { getTimelinePosts } from "../../actions/PostAction";
+import { useParams } from "react-router-dom";
 
 const Posts = () => {
   const dispatch = useDispatch();
+  const params = useParams();
+
   const { user } = useSelector((state) => state.authReducer.authData); // getting the user data from authReducer
-  const { posts, loading } = useSelector((state) => state.postReducer); // getting the post data and loading from postReducer
+  let { posts, loading } = useSelector((state) => state.postReducer); // getting the post data and loading from postReducer
 
   // to fetch posts at the start of our application, we use useEffect
   useEffect(() => {
     dispatch(getTimelinePosts(user._id));
   }, []);
+
+  if (!posts) return "no posts";
+  if (params.id) posts = posts.filter((post) => post.userId === params.id);
 
   return (
     <div className="Posts">
